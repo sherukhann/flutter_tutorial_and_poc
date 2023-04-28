46f636cbbc40c1462d70b154636df53ee141fe06
sherukhann <sheru.khan@byjus.com>
2023-04-28 15:34:06 +0530

    get battery level form native code done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +12,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        brightness: Brightness.light,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 20, color: Colors.red),
-        )
-      ),
+          primarySwatch: Colors.purple,
+          brightness: Brightness.light,
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(fontSize: 20, color: Colors.red),
+          )),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  String batteryLevel = 'UnKnown Battery Level';
+
+  Future<void> getBatteryLevel() async {
+    String bl;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      bl = 'Battery Level at $result %.';
+    } on PlatformException catch (e) {
+      bl = 'Failed to get battery level: ${e.message}';
+    }
+
+    setState(() {
+      batteryLevel = bl;
+    });
+  }
+
   // state full widget
   @override
   Widget build(BuildContext context) {
@@ -36,6 +59,16 @@ class HomePage extends StatelessWidget {
           centerTitle: true,
           // backgroundColor: Colors.green,
         ),
-        body: const Text('learning flutterr'));
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: getBatteryLevel,
+                  child: const Text('Get Battery Level')),
+              Text(batteryLevel)
+            ],
+          ),
+        ));
   }
 }
