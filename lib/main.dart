29e836ad6b081eb2ed60eaa1cf024d7cf043f3ea
sherukhann <sheru.khan@byjus.com>
2023-05-03 12:59:55 +0530

    get and show contact list  done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,31 +15,121 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        brightness: Brightness.light,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 20, color: Colors.red),
-        )
-      ),
+          primarySwatch: Colors.purple,
+          brightness: Brightness.light,
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(fontSize: 20, color: Colors.red),
+          )),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   // state full widget
+  List<Contact> contacts = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getContactsPermission();
+  }
+
+  void getContactsPermission() async {
+    if (await Permission.contacts.isGranted) {
+      // contact fetch
+      fetchContacts();
+    } else {
+      await Permission.contacts.request();
+    }
+  }
+
+  void fetchContacts() async {
+    contacts = await ContactsService.getContacts();
+    debugPrint('contacts length :  ${contacts.length}');
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'my first app',
-          ),
-          centerTitle: true,
-          // backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: const Text(
+          'Contacts',
         ),
-        body: const Text('learning flutterr'));
+        centerTitle: true,
+        // backgroundColor: Colors.green,
+      ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 7,
+                          color: Colors.white.withOpacity(0.1),
+                          offset: const Offset(-3, -3),
+                        ),
+                        BoxShadow(
+                          blurRadius: 7,
+                          color: Colors.black.withOpacity(0.7),
+                          offset: const Offset(3, 3),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(9),
+                      color: Colors.green,
+                    ),
+                    child: Text(
+                      contacts[index].givenName![0],
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    contacts[index].givenName!,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  subtitle: Text(
+                    contacts[index].phones![0].value!,
+                    style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  horizontalTitleGap: 12,
+                );
+              },
+              itemCount: contacts.length,
+            ),
+    );
   }
 }
